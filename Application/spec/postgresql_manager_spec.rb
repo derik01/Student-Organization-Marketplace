7307bf7d718671b05501d56d8bdc6eaaca76505d
derik <derik0194@tamu.edu>
2022-01-05 00:48:55 +0200

    updated for presentation

---
+++ Application/spec/postgresql_manager_spec.rb
@@ -0,0 +1,34 @@
+require "postgresql_manager"
+require "pg_tester"
+ 
+describe PostgresqlManager do
+  psql = PgTester.new({
+          database: "testbuddy",
+          user: "buddy",
+         })
+  before(:each) do
+    psql.setup
+  end
+  after(:each) do
+    psql.teardown
+  end
+  subject { described_class.new(psql.host, psql.database, psql.user, psql.port) }
+  describe "create_new_user" do
+    context "testing create user method" do
+      it "should create a new user" do
+        subject.create_new_user("derik@gmail.com", "password")
+        result = psql.exec "SELECT usename FROM pg_catalog.pg_user"
+        expect(result.values).to include(["derik@gmail.com"])
+      end
+    end
+  end
+  describe "create_new_user" do
+    context "testing create user method" do
+      it "should create a new user" do
+        subject.create_new_user("anukhatri@gmail.com", "password")
+        result2 = psql.exec "SELECT passwd FROM pg_catalog.pg_user"
+        expect(result2.values).to include (["********"])
+      end
+    end
+  end
+end
