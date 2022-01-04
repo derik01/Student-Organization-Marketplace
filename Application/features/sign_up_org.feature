0be247d011cf9743becd4cf6719d07f104440e71
derik <derik0194@tamu.edu>
2022-01-04 12:58:07 +0200

    merged cucumber

--- Application/features/sign_up_org.feature
+++ Application/features/sign_up_org.feature
@@ -8,4 +8,6 @@ Scenario: I sign up as an organization
     Given I am on the home page
     When I press "SIGN UP"
     Then I should be on the sign up page
-    
+    And I should see "Create an Account"
+    Then I sign up with "anukhatri@tamu.edu" username and "Howdy123" password
+    Then I should be on the welcome page
--- Application/features/step_definitions/steps.rb
+++ Application/features/step_definitions/steps.rb
@@ -12,10 +12,21 @@ Given /^(?:|I )am on (.+)$/ do |page_name|
     visit path_to(page_name)
 end
 
-When /^I press "([^\"]*)"$/ do |button|
-    click_button(button)
+When /^I press "([^\"]*)"$/ do |link|
+    click_link(link)
 end
 
 Then /^(?:|I )should be on (.+)$/ do |page_name|
     visit path_to(page_name)
+end
+
+And /^(?:|I )should see "([^\"]*)"$/ do |button__text|
+    page.should have_content(button__text)
+end
+
+Then /^I sign up with "(.*?)" username and "(.*?)" password$/ do |user, password|
+    # find("#user_username").set(user_name)
+    fill_in('user_username', with: user)
+    fill_in('user_password', with: password)
+    click_button("Create Account")
 end
