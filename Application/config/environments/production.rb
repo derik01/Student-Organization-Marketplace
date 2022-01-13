593614cc7238860d1ad6e30127ac5d7385d96655
derik <derik0194@tamu.edu>
2022-01-13 11:54:36 +0200

    fixed delete product route

--- Application/config/environments/production.rb
+++ Application/config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
+  config.read_encrypted_secrets = true
   # Settings specified here will take precedence over those in config/application.rb.
-  config.active_storage.service = :test
+  config.active_storage.service = :amazon
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -25,7 +26,7 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
