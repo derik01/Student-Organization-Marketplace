c00e9f2b04142261986815459e52f63912687d14
Derik Wang <derikwang@Deriks-MacBook-Pro.local>
2022-01-20 11:06:38 -0600

    updated product page

--- Application/db/schema.rb
+++ Application/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_150016) do
+ActiveRecord::Schema.define(version: 2022_01_13_092507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,10 +52,10 @@ ActiveRecord::Schema.define(version: 2022_01_13_150016) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "first"
     t.string "last"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
