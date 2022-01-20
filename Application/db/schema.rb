d6d76438e8a5691da58eed404bab3ff90d6e8ee3
Anu Khatri <anukhatri@tamu.edu>
2022-01-19 19:24:28 -0600

    updated code

--- Application/db/schema.rb
+++ Application/db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2022_01_13_150016) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :serial, force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
